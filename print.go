279825f441407f08351732324d810c7dca263db7
el-ahavah <ihunanyajoyce4@gmail.com>
2026-04-29 01:43:55 +0100

    Update print.go

--- print.go
+++ print.go
@@ -21,7 +21,7 @@ func printAscii(asciiMap map[rune][]string, input string) {
 			fmt.Println()
 			continue
 		}
-		for i := 0; i < 8; i++ {
+		for i := 0; i < height; i++ {
 			for _, ch := range line {
 				if ch < 32 || ch > 126 {
 					ch = ' '
