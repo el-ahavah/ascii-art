11eb90df0a50d9d6596f3316d3f05d7bc6b283f9
el-ahavah <ihunanyajoyce4@gmail.com>
2026-04-29 01:31:05 +0100

    Update build.go

--- build.go
+++ build.go
@@ -6,11 +6,11 @@ import (
 	"strings"
 )
 
-func buildAsciiMap(bannerFile string) map[rune][]string {
+func buildAsciiMap(bannerFile string) (map[rune][]string, error) {
 	file, err := os.ReadFile(bannerFile)
 	if err != nil {
 		fmt.Println("Error reading banner:", err)
-		os.Exit(1)
+		return nil, fmt.Errorf("error reading banner: %w", err)
 	}
 
 	lines := strings.Split(string(file), "\n")
@@ -19,6 +19,10 @@ func buildAsciiMap(bannerFile string) map[rune][]string {
 	for i := 32; i <= 126; i++ {
 		start := (i-32)*9 + 1
 		end := start + 8
+
+		if end > len(lines) {
+			return nil, fmt.Errorf("banner file is malformed or incomplete")
+		}
 
 		asciiMap[rune(i)] = lines[start:end]
 	}
